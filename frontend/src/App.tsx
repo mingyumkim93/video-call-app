9f2f0e68c52ff6d65f351a0594174a0e673ed6e0
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-10-06 14:55:01 +0300

    frontend with typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,16 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  useEffect(() => {
-    axios.get("/api/test").then((res) => console.log(res.data));
-  });
+  axios.get("/api/test").then((res) => console.log(res));
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
