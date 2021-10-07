05589d8f6cd3d48f2a3b8f5ceefa6cc924259004
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-10-07 13:56:28 +0300

    removed unnecessary stuffs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import socket from "./socket";
 
 function App() {
@@ -10,15 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div>app</div>
     </div>
   );
 }
