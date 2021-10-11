0b4e57614ec178a86ae6821a0270cdecc68574ce
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-10-11 13:32:03 +0300

    getting media stream from my device

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -1,5 +1,21 @@
+import { useRef } from "react";
+
 function Room() {
-  return <div>Room</div>;
+  const myVideo = useRef<HTMLVideoElement>(null);
+  async function getMedia() {
+    try {
+      const myStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      myVideo.current!.srcObject = myStream;
+    } catch (e) {}
+  }
+
+  getMedia();
+  return (
+    <div>
+      <h2>Chat room</h2>
+      <video ref={myVideo} width="400" height="300" autoPlay></video>
+    </div>
+  );
 }
 
 export default Room;
