83acb4540fd2c20b207d82c774be3bce0cc83bad
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-10-11 14:37:32 +0300

    toggling medias

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -1,19 +1,46 @@
-import { useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import { ButtonGroup, Button } from "@mui/material";
+import { Mic, MicOff, Videocam, VideocamOff } from "@mui/icons-material";
 
 function Room() {
+  const [myStream, setMyStream] = useState<MediaStream | null>(null);
+  const [isMyCameraOn, setIsMyCameraOn] = useState(true);
+  const [isMyAudioOn, setIsMyAudioOn] = useState(true);
   const myVideo = useRef<HTMLVideoElement>(null);
-  async function getMedia() {
-    try {
-      const myStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-      myVideo.current!.srcObject = myStream;
-    } catch (e) {}
+
+  useEffect(() => {
+    async function getMedia() {
+      try {
+        const userMedia = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        setMyStream(userMedia);
+        if (myVideo.current) myVideo.current.srcObject = userMedia;
+      } catch (e) {}
+    }
+    getMedia();
+  }, []);
+
+  function toggleAudioOn() {
+    setIsMyAudioOn(!isMyAudioOn);
+    if (myStream) myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
   }
 
-  getMedia();
+  function toggleCameraOn() {
+    setIsMyCameraOn(!isMyCameraOn);
+    if (myStream) myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+  }
+
   return (
     <div>
       <h2>Chat room</h2>
       <video ref={myVideo} width="400" height="300" autoPlay></video>
+      <ButtonGroup variant="text" color="inherit">
+        <Button value="mic" onClick={toggleAudioOn}>
+          {isMyAudioOn ? <Mic /> : <MicOff />}
+        </Button>
+        <Button value="camera" onClick={toggleCameraOn}>
+          {isMyCameraOn ? <Videocam /> : <VideocamOff />}
+        </Button>
+      </ButtonGroup>
     </div>
   );
 }
